42e0f436f08778374bc44aa6a0b81d6fd48d6f43
Jarl Stefansson <jarl@sdelements.com>
2013-11-04 16:32:46 -0500

    Added support for pip installed virtualenv facter

--- lib/facter/virtualenv_version.rb
+++ lib/facter/virtualenv_version.rb
@@ -1,7 +1,19 @@
+# Show the virtualenv version
+# works with virualenv loaded and without, pip installed and package installed
 require 'puppet'
 pkg = Puppet::Type.type(:package).new(:name => "virtualenv")
 Facter.add("virtualenv_version") do
+  has_weight 100
   setcode do
-    /^(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    Facter::Util::Resolution.exec('virtualenv --version')
+  end
+end
+
+Facter.add("virtualenv_version") do
+  has_eight 50
+  setcode do
+    if pkg.retrieve[pkg.property(:ensure)] != 'purged'
+        /^.*(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    end
   end
 end
