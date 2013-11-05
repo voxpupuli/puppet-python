28f3371716a5e11433198c41f33a9753e57a003e
Jarl Stefansson <jarl@sdelements.com>
2013-11-05 11:22:59 -0500

    Fix facter for versions

--- lib/facter/python_version.rb
+++ lib/facter/python_version.rb
@@ -5,7 +5,9 @@ pkg = Puppet::Type.type(:package).new(:name => "python")
 
 Facter.add("system_python_version") do
   setcode do
-    /^(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    if pkg.retrieve[pkg.property(:ensure)] != 'purged'
+        /^(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    end
   end
 end
 
