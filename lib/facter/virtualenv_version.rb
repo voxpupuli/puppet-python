c47678748b39c0d7b524e6cfb4b3aab2e8811c6b
Matt Callaway <mcallawa@genome.wustl.edu>
2014-03-04 15:17:07 -0600

    Add begin/rescue/end to setcode block
    This silences these warnings:
    
    Could not retrieve pip_version: undefined method `[]' for nil:NilClass
    Could not retrieve pip_version: can't convert Symbol into String

--- lib/facter/virtualenv_version.rb
+++ lib/facter/virtualenv_version.rb
@@ -5,15 +5,23 @@ pkg = Puppet::Type.type(:package).new(:name => "virtualenv")
 Facter.add("virtualenv_version") do
   has_weight 100
   setcode do
-    Facter::Util::Resolution.exec('virtualenv --version')
+    begin
+      Facter::Util::Resolution.exec('virtualenv --version')
+    rescue
+      false
+    end
   end
 end
 
 Facter.add("virtualenv_version") do
   has_weight 50
   setcode do
-    unless [:absent,'purged'].include?(pkg.retrieve[pkg.property(:ensure)])
-        /^.*(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    begin
+      unless [:absent,'purged'].include?(pkg.retrieve[pkg.property(:ensure)])
+          /^.*(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+      end
+    rescue
+      false
     end
   end
 end
