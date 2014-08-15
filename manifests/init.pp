a4a7c9c4b418c11e2c817ed1c646bff862115acc
Oliver Bertuch <oliver@majinai.net>
2014-08-16 00:37:21 +0200

    adding validation and documentation to init and install, refactored the handling of python::version to be more precise. the code changes are not tested yet, have no spec testing, but need to be synced to my repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,16 @@
 # === Parameters
 #
 # [*version*]
-#  Python version to install. Default: system default
+#  Python version to install. Beware that valid values for this differ a) by
+#  the provider you choose and b) by the osfamily/operatingsystem you are using.
+#  Default: system default
+#  Allowed values:
+#   - provider == pip: everything pip allows as a version after the 'python=='
+#   - else: 'system', 'pypy', 3/3.3/...
+#      - Be aware that 'system' usually means python 2.X.
+#      - 'pypy' actually lets us use pypy as python.
+#      - 3/3.3/... means you are going to install the python3/python3.3/...
+#        package, if available on your osfamily.
 #
 # [*pip*]
 #  Install python-pip. Default: true
@@ -22,6 +31,11 @@
 #
 # [*manage_gunicorn*]
 #  Allow Installation / Removal of Gunicorn. Default: true
+#
+# [*provider*]
+#  What provider to use for installation of the packages, except gunicorn.
+#  Default: system default provider
+#  Allowed values: 'pip'
 #
 # === Examples
 #
@@ -46,6 +60,24 @@ class python (
   $manage_gunicorn = true,
   $provider        = undef
 ) {
+
+  # validate inputs
+  if $provider != undef {
+    validate_re($provider, ['^pip$'], 'Only "pip" is a valid provider besides the system default.')
+  }
+
+  if $provider == 'pip' {
+    validate_re($version, '^(2\.[4-7]\.\d|3\.\d\.\d)$') 
+  # this will only be checked if not pip, every other string would be rejected by provider check 
+  } else {
+    validate_re($version, concat(['system', 'pypy'], $::python::install::valid_versions]))
+  }
+
+  validate_bool($pip)
+  validate_bool($dev)
+  validate_bool($virtualenv)
+  validate_bool($gunicorn)
+  validate_bool($manage_gunicorn)
 
   # Module compatibility check
   $compatible = [ 'Debian', 'RedHat']
