246b1a047953b5110e123b665d5afaec33ddef64
Gregory Saramite <gregory@skout.com>
2014-10-17 11:25:39 -0700

    Added python::params & fixed $valid_versions def

---
+++ manifests/params.pp
@@ -0,0 +1,17 @@
+
+# Class: python::params
+# The python Module default configuration settings.
+#
+class python::params {
+  $version                = 'system'
+  $pip                    = true
+  $dev                    = false
+  $virtualenv             = false
+  $gunicorn               = false
+  $manage_gunicorn        = true
+  $provider               = undef
+  $valid_versions = $::osfamily ? {
+    'RedHat' => ['3'],
+    'Debian' => ['3', '3.3'],
+  }
+}
