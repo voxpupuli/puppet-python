ecf53627c856169ec4054f0687c69c32f3618300
Gene Wood <gene_wood@cementhorizon.com>
2015-06-13 12:24:43 -0700

    Fix puppet-lint issue from f6d4b30

--- manifests/virtualenv.pp
+++ manifests/virtualenv.pp
@@ -176,14 +176,14 @@ define python::virtualenv (
       }
 
       python::requirements { "${requirements}_${venv_dir}":
-        requirements    => $requirements,
-        virtualenv      => $venv_dir,
-        proxy           => $proxy,
-        owner           => $owner,
-        group           => $group,
-        cwd             => $cwd,
-        require         => Exec["python_virtualenv_${venv_dir}"],
-        extra_pip_args  => $extra_pip_args,
+        requirements   => $requirements,
+        virtualenv     => $venv_dir,
+        proxy          => $proxy,
+        owner          => $owner,
+        group          => $group,
+        cwd            => $cwd,
+        require        => Exec["python_virtualenv_${venv_dir}"],
+        extra_pip_args => $extra_pip_args,
       }
     }
   } elsif $ensure == 'absent' {
