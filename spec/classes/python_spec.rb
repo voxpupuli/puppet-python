bd3bef1a82194c72ad4c7c3d9c8780d349e3e856
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-06-19 11:30:40 +0200

    Add support for SLES
    Besides added Support, add specs for SLES, and update metadata.json
    to reflect the support.

--- spec/classes/python_spec.rb
+++ spec/classes/python_spec.rb
@@ -61,7 +61,7 @@ describe 'python', :type => :class do
       
       # python::provider
       context "default" do
-        let (:params) {{ :provider => '' }} 
+        let (:params) {{ :provider => '' }}
         it { is_expected.to contain_package("python-virtualenv")}
         it { is_expected.to contain_package("python-pip")}
         
@@ -181,4 +181,94 @@ describe 'python', :type => :class do
       end
     end
   end
-end 
+
+  context "on a SLES 11 SP3" do
+    let :facts do
+      {
+        :id => 'root',
+        :kernel => 'Linux',
+        :osfamily => 'Suse',
+        :operatingsystem => 'SLES',
+        :operatingsystemrelease => '11.3',
+        :concat_basedir => '/dne',
+        :path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      }
+    end
+    it { is_expected.to contain_class("python::install") }
+    # Base Suse packages.
+    it { is_expected.to contain_package("python") }
+    it { is_expected.to contain_package("python-devel") }
+    it { is_expected.to contain_package("python-pip") }
+    # Basic python packages (from pip)
+    it { is_expected.to contain_package("python-virtualenv")}
+
+    describe "with python::dev" do
+      context "true" do
+        let (:params) {{ :dev => true }}
+        it { is_expected.to contain_package("python-devel").with_ensure('present') }
+      end
+      context "empty/default" do
+        it { is_expected.to contain_package("python-devel").with_ensure('absent') }
+      end
+    end
+
+    describe "with manage_gunicorn" do
+      context "true" do
+        let (:params) {{ :manage_gunicorn => true }}
+        it { is_expected.to contain_package("gunicorn") }
+      end
+      context "empty args" do
+        #let (:params) {{ :manage_gunicorn => '' }}
+        it { is_expected.to contain_package("gunicorn") }
+      end
+      context "false" do
+        let (:params) {{ :manage_gunicorn => false }}
+        it {is_expected.not_to contain_package("gunicorn")}
+      end
+    end
+
+    describe "with python::provider" do
+      context "pip" do
+        let (:params) {{ :provider => 'pip' }}
+
+        it { is_expected.to contain_package("virtualenv").with(
+          'provider' => 'pip'
+        )}
+        it { is_expected.to contain_package("pip").with(
+          'provider' => 'pip'
+        )}
+      end
+
+      # python::provider
+      context "default" do
+        let (:params) {{ :provider => '' }}
+        it { is_expected.to contain_package("python-virtualenv")}
+        it { is_expected.to contain_package("python-pip")}
+
+        describe "with python::virtualenv" do
+          context "true" do
+            let (:params) {{ :provider => '', :virtualenv => true }}
+            it { is_expected.to contain_package("python-virtualenv").with_ensure('present') }
+          end
+        end
+
+        describe "with python::virtualenv" do
+          context "default/empty" do
+            let (:params) {{ :provider => '' }}
+            it { is_expected.to contain_package("python-virtualenv").with_ensure('absent') }
+          end
+        end
+      end
+    end
+
+    describe "with python::dev" do
+      context "true" do
+        let (:params) {{ :dev => true }}
+        it { is_expected.to contain_package("python-devel").with_ensure('present') }
+      end
+      context "default/empty" do
+        it { is_expected.to contain_package("python-devel").with_ensure('absent') }
+      end
+    end
+  end
+end
