58d5cb23fcf4fa11e641ba8204f164a5c6bfa043
dannygoulder <github@daniel.goulder.name>
2016-10-04 20:49:29 +0400

    Make proper indentation :)

--- manifests/install.pp
+++ manifests/install.pp
@@ -178,7 +178,7 @@ class python::install {
       package { 'python-dev':
         ensure => $dev_ensure,
         name   => $pythondev,
-		alias  => $pythondev,
+        alias  => $pythondev,
       }
 
       if $::osfamily == 'RedHat' {
