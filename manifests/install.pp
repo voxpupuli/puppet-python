50cf052d79a717ab1ecd3948434e4dd55d03bca7
Ira Abramov <ira.abramov@ownbackup.com>
2017-01-25 10:50:31 +0200

    Fix the package name (Quick and ugly, no support for Python 3) for Amazon Linux

--- manifests/install.pp
+++ manifests/install.pp
@@ -213,6 +213,9 @@ class python::install {
       if "${::python::version}" =~ /^3/ { #lint:ignore:only_variable_string
         $pip_category = undef
         $pip_package = 'python3-pip'
+      } elsif $::operatingsystem == "Amazon" {
+        $pip_category = undef
+        $pip_package = 'python27-pip'
       } elsif ($::osfamily == 'RedHat') and (versioncmp($::operatingsystemmajrelease, '7') >= 0) {
         $pip_category = undef
         $pip_package = 'python2-pip'
