efd6379ec26d20abc931af0cc181d7d3799b705d
Tim Meusel <tim@bastelfreak.de>
2018-05-12 23:22:55 +0200

    migrate pyvenv_spec.rb to rspec-puppet-facts

--- spec/defines/pyvenv_spec.rb
+++ spec/defines/pyvenv_spec.rb
@@ -1,27 +1,33 @@
 require 'spec_helper'
 
-describe 'python::pyvenv', :type => :define do
-  let (:title) { '/opt/env' }
-  let (:facts) do
-    {
-      :lsbdistcodename => 'jessie',
-      :osfamily => 'Debian',
-    }
-  end
+describe 'python::pyvenv', type: :define do
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
+      let :title do
+        '/opt/env'
+      end
 
-  it {
-    is_expected.to contain_file( '/opt/env')
-    is_expected.to contain_exec( "python_virtualenv_/opt/env").with_command("pyvenv --clear  /opt/env")
-  }
-
-  describe 'when ensure' do
-    context "is absent" do
-      let (:params) {{
-        :ensure => 'absent'
-      }}
       it {
-        is_expected.to contain_file( '/opt/env').with_ensure('absent').with_purge( true)
+        is_expected.to contain_file('/opt/env')
+        is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('pyvenv --clear  /opt/env')
       }
+
+      describe 'when ensure' do
+        context 'is absent' do
+          let :params do
+            {
+              ensure: 'absent'
+            }
+          end
+
+          it {
+            is_expected.to contain_file('/opt/env').with_ensure('absent').with_purge(true)
+          }
+        end
+      end
     end
   end
 end
