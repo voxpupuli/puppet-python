bbb85bf0f4cf34112adae0c89b527168bef53e38
Vincent Brillault <vincent.brillault@cern.ch>
2018-09-07 12:41:17 +0200

    Acceptance test: check if pip installation is idempotent
    Second part of regression test for PR#418

--- spec/acceptance/virtualenv_spec.rb
+++ spec/acceptance/virtualenv_spec.rb
@@ -53,6 +53,7 @@ describe 'python class' do
 
       # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
   end
 end
