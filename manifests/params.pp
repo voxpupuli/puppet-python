8395eefa60c04ca7212f056c40b86ddd9edfc923
Jean Michel Feltrin <jean.feltrin@velocitycloud.com>
2019-01-11 11:11:10 -0200

    allow to bootstrap pip without manage all python packages

--- manifests/params.pp
+++ manifests/params.pp
@@ -27,6 +27,11 @@ class python::params {
     $use_epel             = false
   }
 
+  $pip_lookup_path = $::osfamily ? {
+    'AIX' => [ '/bin', '/usr/bin', '/usr/local/bin', '/opt/freeware/bin/' ],
+    default => [ '/bin', '/usr/bin', '/usr/local/bin' ]
+  }
+
   $gunicorn_package_name = $::osfamily ? {
     'RedHat' => 'python-gunicorn',
     default  => 'gunicorn',
