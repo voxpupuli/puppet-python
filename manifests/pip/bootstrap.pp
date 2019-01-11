7be8dcb5c9f4b35f49a41f686cb1a1c71b8b319b
Jean Michel Feltrin <jean.feltrin@velocitycloud.com>
2019-01-11 13:30:22 -0200

    adjust pip module

---
+++ manifests/pip/bootstrap.pp
@@ -0,0 +1,52 @@
+#
+# @summary allow to bootstrap pip when python is managed from other module
+#
+# @param version should be pip or pip3
+# @param manage_python if python module will manage deps
+#
+# @example 
+#   class { 'python::pip::bootstrap':
+#     version => 'pip',
+#   }
+class python::pip::bootstrap (
+  Enum['pip', 'pip3'] $version            = 'pip',
+  Variant[Boolean, String] $manage_python = false,
+) inherits ::python::params {
+  if $manage_python {
+    ::include python
+  } else {
+    $target_src_pip_path = $osfamily ? {
+      'AIX' => '/opt/freeware/bin',
+      default => '/usr/bin'
+    }
+    if $version == 'pip3' {
+      exec { 'bootstrap pip3':
+        command => '/usr/bin/curl https://bootstrap.pypa.io/get-pip.py | python3',
+        unless  => 'which pip3',
+        path    => $python::params::pip_lookup_path,
+        require => Package['python3'],
+      }
+      # puppet is opinionated about the pip command name
+      file { 'pip3-python':
+        ensure  => link,
+        path    => '/usr/bin/pip3',
+        target  => "${target_src_pip_path}/pip${::facts['python3_release']}",
+        require => Exec['bootstrap pip3'],
+      }
+    } else {
+        exec { 'bootstrap pip':
+          command => '/usr/bin/curl https://bootstrap.pypa.io/get-pip.py | python',
+          unless  => 'which pip',
+          path    => $python::params::pip_lookup_path,
+          require => Package['python'],
+        }
+        # puppet is opinionated about the pip command name
+        file { 'pip-python':
+          ensure  => link,
+          path    => '/usr/bin/pip',
+          target  => "${target_src_pip_path}/pip${::facts['python2_release']}",
+          require => Exec['bootstrap pip'],
+        }
+    }
+  }
+
