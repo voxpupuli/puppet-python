c881d2e128a3e6af930fadffdfb8c4ed8731917b
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2019-10-28 19:12:56 +0100

    Recognize Debian python package name, fixes: #506
    Apply the suggested fix to recognize Debian python pacakges.

--- manifests/init.pp
+++ manifests/init.pp
@@ -65,7 +65,7 @@ class python (
     default => '',
   }
 
-  unless $version =~ Pattern[/\A(python)?[0-9](\.?[0-9])+/,
+  unless $version =~ Pattern[/\A(python)?[0-9](\.?[0-9])*/,
         /\Apypy\Z/, /\Asystem\Z/, /\Arh-python[0-9]{2}(?:-python)?\Z/] {
     fail("version needs to be pypy, system or a version string like '36', '3.6' or 'python3.6' )")
   }
