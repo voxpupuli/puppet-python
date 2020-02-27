56f519944f3a1469a6f23976524f6ab454f70e30
Philip Jones <53430719+pjonesIDBS@users.noreply.github.com>
2020-02-27 14:51:24 +0000

    Fixes version check for virtualenv_version
    This should fix issue #534 I've tested locally with success. I've tested against Virtualenv 20.0.4 and 16.7.9.
    The output from the fact is only the version number as was previously the case.

--- spec/unit/facter/virtualenv_version_spec.rb
+++ spec/unit/facter/virtualenv_version_spec.rb
@@ -5,19 +5,44 @@ describe Facter::Util::Fact do
     Facter.clear
   end
 
-  let(:virtualenv_version_output) do
+  let(:virtualenv_old_version_output) do
     <<-EOS
 12.0.7
 EOS
   end
 
-  describe 'virtualenv_version' do
+  let(:virtualenv_new_version_output) do
+    <<-EOS
+virtualenv 20.0.17 from /opt/python/lib/python3.5/site-packages/virtualenv/__init__.py
+EOS
+  end
+
+  describe 'virtualenv_version old' do
     context 'returns virtualenv version when virtualenv present' do
       it do
         Facter::Util::Resolution.stubs(:exec)
         Facter::Util::Resolution.expects(:which).with('virtualenv').returns(true)
-        Facter::Util::Resolution.expects(:exec).with('virtualenv --version 2>&1').returns(virtualenv_version_output)
+        Facter::Util::Resolution.expects(:exec).with('virtualenv --version 2>&1').returns(virtualenv_old_version_output)
         expect(Facter.value(:virtualenv_version)).to eq('12.0.7')
+      end
+    end
+
+    context 'returns nil when virtualenv not present' do
+      it do
+        Facter::Util::Resolution.stubs(:exec)
+        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(false)
+        expect(Facter.value(:virtualenv_version)).to eq(nil)
+      end
+    end
+  end
+
+  describe 'virtualenv_version new' do
+    context 'returns virtualenv version when virtualenv present' do
+      it do
+        Facter::Util::Resolution.stubs(:exec)
+        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(true)
+        Facter::Util::Resolution.expects(:exec).with('virtualenv --version 2>&1').returns(virtualenv_new_version_output)
+        expect(Facter.value(:virtualenv_version)).to eq('20.0.17')
       end
     end
 
