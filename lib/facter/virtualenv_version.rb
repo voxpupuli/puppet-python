99ab8cc93ca66e5cb1819e96856846e7a9230b3c
Wiebe Verweij <wiebe.verweij@moxio.com>
2020-07-21 12:54:38 +0200

    Improve handling of missing output of virtualenv version fact command
    Partially fixes #527
    
    Signed-off-by: Wiebe Verweij <wiebe.verweij@moxio.com>

--- lib/facter/virtualenv_version.rb
+++ lib/facter/virtualenv_version.rb
@@ -3,7 +3,8 @@
 Facter.add('virtualenv_version') do
   setcode do
     if Facter::Util::Resolution.which('virtualenv')
-      Facter::Util::Resolution.exec('virtualenv --version 2>&1').match(%r{(\d+\.\d+\.?\d*).*$})[1]
+      results = Facter::Util::Resolution.exec('virtualenv --version 2>&1').match(%r{(\d+\.\d+\.?\d*).*$})
+      results[1] if results
     end
   end
 end
