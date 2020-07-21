ce29405c6df7ac1e1912f03fcb85229efb296eee
Wiebe Verweij <wiebe.verweij@moxio.com>
2020-07-21 12:48:38 +0200

    Add pip2 and pip3 version facts and improve handling of missing output
    Partly fixes #527.
    
    Signed-off-by: Wiebe Verweij <wiebe.verweij@moxio.com>

--- lib/facter/pip_version.rb
+++ lib/facter/pip_version.rb
@@ -1,9 +1,26 @@
 # Make pip version available as a fact
 
+def get_pip_version(executable)
+  if Facter::Util::Resolution.which(executable) # rubocop:disable Style/GuardClause
+    results = Facter::Util::Resolution.exec("#{executable} --version 2>&1").match(%r{^pip (\d+\.\d+\.?\d*).*$})
+    results[1] if results
+  end
+end
+
 Facter.add('pip_version') do
   setcode do
-    if Facter::Util::Resolution.which('pip')
-      Facter::Util::Resolution.exec('pip --version 2>&1').match(%r{^pip (\d+\.\d+\.?\d*).*$})[1]
-    end
+    get_pip_version 'pip'
+  end
+end
+
+Facter.add('pip2_version') do
+  setcode do
+    get_pip_version 'pip2'
+  end
+end
+
+Facter.add('pip3_version') do
+  setcode do
+    get_pip_version 'pip3'
   end
 end
