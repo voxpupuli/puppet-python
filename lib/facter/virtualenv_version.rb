47e2fa41c5d3bca478c75a726b97f2ca05d40016
GusPriceAtlassian <68916486+GusPriceAtlassian@users.noreply.github.com>
2020-07-31 11:42:00 -0700

    Update virtualenv_version.rb
    This should be a 0

--- lib/facter/virtualenv_version.rb
+++ lib/facter/virtualenv_version.rb
@@ -3,7 +3,7 @@
 Facter.add('virtualenv_version') do
   setcode do
     if Facter::Util::Resolution.which('virtualenv')
-      Facter::Util::Resolution.exec('virtualenv --version 2>&1').match(%r{(\d+\.\d+\.?\d*).*$})[1]
+      Facter::Util::Resolution.exec('virtualenv --version 2>&1').match(%r{(\d+\.\d+\.?\d*).*$})[0]
     end
   end
 end
