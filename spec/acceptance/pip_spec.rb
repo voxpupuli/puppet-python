f36ea4abe4499fd6ef7a99ecd8eaf603ab0a4bef
BR <b.rabiega@gmail.com>
2021-04-28 22:18:04 +0200

    Use virtualenv for pip acceptance tests

--- spec/acceptance/pip_spec.rb
+++ spec/acceptance/pip_spec.rb
@@ -9,9 +9,17 @@ describe 'python::pip defined resource' do
         dev     => 'present',
       }
 
+      python::pyvenv { '/opt/test-venv':
+        ensure      => 'present',
+        systempkgs  => false,
+        mode        => '0755',
+        pip_version => '<= 20.3.4',
+      }
+
       python::pip { 'agent package':
-        pkgname => 'agent',
-        ensure  => '0.1.2',
+        virtualenv => '/opt/test-venv',
+        pkgname    => 'agent',
+        ensure     => '0.1.2',
       }
       PUPPET
 
@@ -20,7 +28,7 @@ describe 'python::pip defined resource' do
     end
   end
 
-  describe command('/usr/bin/pip list') do
+  describe command('/opt/test-venv/bin/pip list') do
     its(:exit_status) { is_expected.to eq 0 }
     its(:stdout) { is_expected.to match %r{agent.* 0\.1\.2} }
   end
@@ -33,25 +41,33 @@ describe 'python::pip defined resource' do
         dev     => 'present',
       }
 
-      python::pip { 'agent package install':
-        pkgname => 'agent',
-        ensure  => '0.1.2',
+      python::pyvenv { '/opt/test-venv':
+        ensure      => 'present',
+        systempkgs  => false,
+        mode        => '0755',
+        pip_version => '<= 20.3.4',
       }
 
-      python::pip { 'agent package uninstall':
-        pkgname => 'agent',
+      python::pip { 'agent package install':
+        ensure     => '0.1.2',
+        pkgname    => 'agent',
+        virtualenv => '/opt/test-venv',
+      }
+
+      python::pip { 'agent package uninstall custom pkgname':
         ensure  => 'absent',
+        pkgname => 'agent',
+        virtualenv => '/opt/test-venv',
         require => Python::Pip['agent package install'],
       }
 
       PUPPET
 
       apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
     end
   end
 
-  describe command('/usr/bin/pip list') do
+  describe command('/opt/test-venv/bin/pip list') do
     its(:exit_status) { is_expected.to eq 0 }
     its(:stdout) { is_expected.not_to match %r{agent.* 0\.1\.2} }
   end
