4523bd7dcc2c11a0f171d48c97c7a6bd2c93c4c0
Lushaj, Valdrin <Valdrin.Lushaj@bottomline.com>
2023-11-27 17:50:27 +0100

    Add Support for RedHat/CentOS 9

--- manifests/params.pp
+++ manifests/params.pp
@@ -28,6 +28,7 @@ class python::params {
 
   $gunicorn_package_name = $facts['os']['family'] ? {
     'RedHat' => $facts['os']['release']['major'] ? {
+      '9' => 'python3-gunicorn',
       '8' => 'python3-gunicorn',
       default => 'python-gunicorn',
     },
