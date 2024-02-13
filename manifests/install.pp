931bd693fa43aa481647a6326e67761090ff4206
Elliot Schleglemilch <elliot@schlegelmilch.org>
2024-02-13 12:22:24 -0930

    fix double quoted string containing no variables.

--- manifests/install.pp
+++ manifests/install.pp
@@ -222,7 +222,7 @@ class python::install {
         $pip_package  = 'python2-pip'
         $pip_provider = pip2
       } elsif $facts['os']['family'] == 'FreeBSD' {
-        $pip_package = sprintf( "py%s-pip", regsubst($python::version, '([0-9])\\.([0-9]+)', '\\1\\2') )
+        $pip_package = sprintf( 'py%s-pip', regsubst($python::version, '([0-9])\\.([0-9]+)', '\\1\\2') )
         $pip_provider = 'pip'
       } elsif $facts['os']['family'] == 'Gentoo' {
         $pip_package  = 'dev-python/pip'
