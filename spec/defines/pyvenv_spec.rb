81daea252076379ab869761040f0c86bbc9e7488
Romain Tartière <romain@blogreen.org>
2025-04-24 11:11:07 -1000

    Add a unit test to illustrate the pyvenv bootstrap issue
    This module that manage python needs python to be installed in order to
    work properly under certain conditions.
    
    When python is not installed on a node, the python_version fact does not
    have a value.  When using `python::pyvenv` on such a system without a
    specific version set, a call of the split() function on this unde value
    cause an error te be raised:
    
    ```
       Error: Evaluation Error: Error while evaluating a Resource Statement, Evaluation Error: Error while evaluating a Function Call, The function 'split' was called with arguments it does not accept. It expects one of:
        (String str, String pattern)
          rejected: parameter 'str' expects a String value, got Undef
        (String str, Regexp pattern)
          rejected: parameter 'str' expects a String value, got Undef
        (String str, Type[Regexp] pattern)
          rejected: parameter 'str' expects a String value, got Undef
        (Sensitive[String] sensitive, String pattern)
          rejected: parameter 'sensitive' expects a Sensitive[String] value, got Undef
        (Sensitive[String] sensitive, Regexp pattern)
          rejected: parameter 'sensitive' expects a Sensitive[String] value, got Undef
        (Sensitive[String] sensitive, Type[Regexp] pattern)
          rejected: parameter 'sensitive' expects a Sensitive[String] value, got Undef (file: /etc/puppetlabs/code/environments/production/modules/python/manifests/pyvenv.pp, line: 48, column: 34) (file: /etc/puppetlabs/code/environments/production/modules/taiga/manifests/back/install.pp, line: 15) on node debian12-64-puppet7.example.com
    ```

--- spec/defines/pyvenv_spec.rb
+++ spec/defines/pyvenv_spec.rb
@@ -6,83 +6,94 @@ describe 'python::pyvenv', type: :define do
   on_supported_os.each do |os, facts|
     next if os == 'gentoo-3-x86_64'
 
+    let :title do
+      '/opt/env'
+    end
+
     context "on #{os}" do
-      let :facts do
-        # python3 is required to use pyvenv
-        facts.merge(
-          python3_version: '3.5.1'
-        )
-      end
-      let :title do
-        '/opt/env'
-      end
-
       context 'with default parameters' do
-        it { is_expected.to contain_file('/opt/env').that_requires('Class[python::install]') }
-        it { is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('pyvenv-3.5 --clear   /opt/env && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade pip && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade setuptools') }
+        let :facts do
+          facts
+        end
+
+        it { is_expected.to compile }
       end
 
-      describe 'when ensure' do
-        context 'is absent' do
+      context 'with a specific python3 version' do
+        let :facts do
+          # python3 is required to use pyvenv
+          facts.merge(
+            python3_version: '3.5.1'
+          )
+        end
+
+        context 'with default parameters' do
+          it { is_expected.to contain_file('/opt/env').that_requires('Class[python::install]') }
+          it { is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('pyvenv-3.5 --clear   /opt/env && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade pip && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade setuptools') }
+        end
+
+        describe 'when ensure' do
+          context 'is absent' do
+            let :params do
+              {
+                ensure: 'absent'
+              }
+            end
+
+            it {
+              expect(subject).to contain_file('/opt/env').with_ensure('absent').with_purge(true)
+            }
+          end
+        end
+      end
+
+      context "prompt on #{os} with python 3.6" do
+        let :facts do
+          # python 3.6 is required for venv and prompt
+          facts.merge(
+            python3_version: '3.6.1'
+          )
+        end
+        let :title do
+          '/opt/env'
+        end
+
+        context 'with prompt' do
           let :params do
             {
-              ensure: 'absent'
+              prompt: 'custom prompt',
             }
           end
 
           it {
-            expect(subject).to contain_file('/opt/env').with_ensure('absent').with_purge(true)
+            is_expected.to contain_file('/opt/env').that_requires('Class[python::install]')
+            is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('python3.6 -m venv --clear  --prompt custom\\ prompt /opt/env && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade pip && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade setuptools')
           }
         end
       end
-    end
 
-    context "prompt on #{os} with python 3.6" do
-      let :facts do
-        # python 3.6 is required for venv and prompt
-        facts.merge(
-          python3_version: '3.6.1'
-        )
-      end
-      let :title do
-        '/opt/env'
-      end
-
-      context 'with prompt' do
-        let :params do
-          {
-            prompt: 'custom prompt',
-          }
+      context "prompt on #{os} with python 3.5" do
+        let :facts do
+          facts.merge(
+            python3_version: '3.5.1'
+          )
+        end
+        let :title do
+          '/opt/env'
         end
 
-        it {
-          is_expected.to contain_file('/opt/env').that_requires('Class[python::install]')
-          is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('python3.6 -m venv --clear  --prompt custom\\ prompt /opt/env && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade pip && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade setuptools')
-        }
-      end
-    end
+        context 'with prompt' do
+          let :params do
+            {
+              prompt: 'custom prompt',
+            }
+          end
 
-    context "prompt on #{os} with python 3.5" do
-      let :facts do
-        facts.merge(
-          python3_version: '3.5.1'
-        )
-      end
-      let :title do
-        '/opt/env'
-      end
-
-      context 'with prompt' do
-        let :params do
-          {
-            prompt: 'custom prompt',
+          it {
+            is_expected.to contain_file('/opt/env').that_requires('Class[python::install]')
+            is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('pyvenv-3.5 --clear   /opt/env && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade pip && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade setuptools')
           }
         end
-
-        it {
-          is_expected.to contain_file('/opt/env').that_requires('Class[python::install]')
-          is_expected.to contain_exec('python_virtualenv_/opt/env').with_command('pyvenv-3.5 --clear   /opt/env && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade pip && /opt/env/bin/pip --log /opt/env/pip.log install --upgrade setuptools')
-        }
       end
     end
   end
