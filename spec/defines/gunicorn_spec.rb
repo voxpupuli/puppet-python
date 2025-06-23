91d351ecc6ab04668d2e347b5d9b62d80a2d9799
Calle Englund <calle.englund@liu.se>
2025-06-23 17:20:14 +0200

    Expect current default for gunicorn workers
    Given that legacy facts were removed from facterdb 3, the current
    behavior relies on `NilClass#to_i` resolving to missing fact to 0.
    
    The added test case for custom `workers` parameter given cannot give
    false positives with the default formula always being an odd number.

--- spec/defines/gunicorn_spec.rb
+++ spec/defines/gunicorn_spec.rb
@@ -12,8 +12,13 @@ describe 'python::gunicorn', type: :define do
       describe 'test-app with default parameter values' do
         context 'configures test app with default parameter values' do
           let(:params) { { dir: '/srv/testapp' } }
+          let(:expected_workers) do
+            # templates/gunicorn.erb
+            (facts[:processorcount].to_i * 2) + 1
+          end
 
           it { is_expected.to contain_file('/etc/gunicorn.d/test-app').with_mode('0644').with_content(%r{--log-level=error}) }
+          it { is_expected.to contain_file('/etc/gunicorn.d/test-app').with_mode('0644').with_content(%r{--workers=#{expected_workers}}) }
         end
 
         context 'test-app with custom log level' do
@@ -26,6 +31,12 @@ describe 'python::gunicorn', type: :define do
           let(:params) { { dir: '/srv/testapp', args: ['--preload'] } }
 
           it { is_expected.to contain_file('/etc/gunicorn.d/test-app').with_mode('0644').with_content(%r{--preload}) }
+        end
+
+        context 'test-app with custom workers count' do
+          let(:params) { { dir: '/srv/testapp', workers: 42 } }
+
+          it { is_expected.to contain_file('/etc/gunicorn.d/test-app').with_mode('0644').with_content(%r{--workers=#{params[:workers]}}) }
         end
       end
     end
